6a0f23b4e42d63ff1357e375af4d7fb2788ee430
yash chotaliya <yashchotaliya187@gmail.com>
2024-03-13 12:22:18 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,81 @@
+from dotenv import load_dotenv
+load_dotenv() # load all the environment variable from .env
+
+import streamlit as st
+import os
+from PIL import Image
+import google.generativeai as genai
+import prompts
+import requests
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# load gemini pro vision model
+model = genai.GenerativeModel('gemini-pro-vision')
+
+def get_gemini_response(input, image, user_prompt):
+    response = model.generate_content([input, image, user_prompt])
+    return response.text
+
+def get_image_data(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        image_parts=[
+            {
+                "mime_type": uploaded_file.type,
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("File Not Uploaded")
+    
+def generate_invoice(header, items, total_amount):
+    
+    res = requests.post("http://127.0.0.1:5000/test", data={"header": header, "items": items, "total_amount": total_amount})
+    
+    if(res.status_code != 200):
+        return "Error Generating Invoice"
+    
+    return res.content.decode("utf-8")
+
+if __name__ == "__main__":
+    # initialize streamlit app
+
+    st.title("Invoice Generator")
+    
+    st.image("inv_1.png", use_column_width=True, caption="format of generated invoice")
+    
+    uploaded_file = st.file_uploader("Upload an image of Invoice", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        
+    submit = st.button("submit")
+        
+    if submit:
+        if uploaded_file is not None:
+            
+            image_data=get_image_data(uploaded_file)
+            
+            input_prompt = """
+            you are an expert in understanding invoices. we will 
+            upload an image as invoice and you will
+            have to answer any question based on 
+            the uploaded invoice image"""
+            
+            try:
+                header = get_gemini_response(input_prompt, image_data[0], prompts.header_prompt)[10:-4].strip()
+                items = get_gemini_response(input_prompt, image_data[0], prompts.items_prompt).strip()
+                total_amount = get_gemini_response(input_prompt, image_data[0], prompts.total_amount_prompt).strip()
+                
+                link = generate_invoice(header, items, total_amount)
+                if(link!=None):    
+                    st.write("Invoice generated, you can download it by clicking below button")
+                    st.link_button("download invoice", url=link)
+            except:
+                st.error("Error Generating Invoice")
+            
+        else:
+            st.write("please upload an image of invoice")
---
+++ prompts.py
@@ -0,0 +1,5 @@
+header_prompt = "give company name,  company address,  company mobile/phone number,  date,  invoice no, customer name, customer mobile/phone number, customer address in the form of python dictionary with keys [company name,  company address,  company number,  date,  invoice no, customer name, customer number, customer address] and without dictionary name"
+
+items_prompt = "give serial no, item name, qty, rate, amount details of all item in the form of python 2d list without list name"
+
+total_amount_prompt = "give total payable amount of invoice with fraction number without variable name"
