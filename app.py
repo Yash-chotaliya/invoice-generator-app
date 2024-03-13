d143d9d548419b6eb2698a2768afff6931ebe7b5
yash chotaliya <yashchotaliya187@gmail.com>
2024-03-13 18:35:15 +0530

    invoice generated successfully

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 import google.generativeai as genai
 import prompts
-import webbrowser
+import asyncio
 import requests
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
@@ -31,9 +31,12 @@ def get_image_data(uploaded_file):
     else:
         raise FileNotFoundError("File Not Uploaded")
     
-def generate_invoice(header, items, total_amount):
+async def generate_invoice(header, items, total_amount) :
     
     res = requests.post("https://invoice-generator-api-jklu.onrender.com/test", data={"header": header, "items": items, "total_amount": total_amount})
+    
+    if res.status_code != 200:
+        return ""
     
     return res.content.decode("utf-8")
 
@@ -55,6 +58,8 @@ if __name__ == "__main__":
     if submit:
         if uploaded_file is not None:
             
+            link = ""
+            
             image_data=get_image_data(uploaded_file)
             
             input_prompt = """
@@ -68,14 +73,17 @@ if __name__ == "__main__":
                 items = get_gemini_response(input_prompt, image_data[0], prompts.items_prompt).strip()
                 total_amount = get_gemini_response(input_prompt, image_data[0], prompts.total_amount_prompt).strip()
                 
-                link = generate_invoice(header, items, total_amount)
-                if(link!=None):    
+                link = asyncio.run(generate_invoice(header, items, total_amount))
+                
+                if(link!=""):    
                     st.write("Invoice generated, you can download it by clicking below button")
                     st.markdown(f'''<a href="{link}"><button style="background-color:Green;">Download</button></a>''',
                     unsafe_allow_html=True)
+                else:
+                    st.error("Error Generating Invoice")
             except:
                 st.error("Error Generating Invoice")
             
         else:
-            st.write("please upload an image of invoice")
+            st.error("please upload an image of invoice")
 
